44f1d5b90793c29e8fd6f99901f6fbc2b2fcbf57
github username <github email address>
2018-09-26 17:39:56 -0400

    remove messy coding

--- index.js
+++ index.js
@@ -1,68 +1,96 @@
 const URL = 'http://localhost:3000/books'
 const book_list = document.querySelector("#list")
 const show_list = document.querySelector("#show-panel")
-document.addEventListener("DOMContentLoaded", function() {
-  get(URL).then(res => res.forEach(function(element){
-    let book = document.createElement("li")
-    book.innerHTML = element.title
-    book_list.append(book)
 
-    book.addEventListener("click", function(){
-      let button = document.createElement("button")
-      button.innerHTML = "Like"
-      let arr_user = render(element, button)
-      button.addEventListener("click",function(){
-        fetch(`${URL}/${element.id}`,{
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            // "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: JSON.stringify(patch(arr_user))
-        }).then(response => response.json());
-        let u_li = document.createElement("li")
-        u_li.innerHTML = "pouros"
-        document.querySelector("#ul1").append(u_li)
-      })
 
-    })
-  }))
-
+document.addEventListener("DOMContentLoaded", function(){
+  get(URL).then(res => {
+    render_book_list(res)
+  })
 
 });
 
-function render(element, button){
-  show_list.innerHTML = ""
-  let show = document.createElement("div")
-  show.innerHTML = `
-  <img src=${element.img_url}>
-  <p>${element.description}</p>
-  `
-  let u_ul = document.createElement("ul")
-  u_ul.setAttribute("id", "ul1")
-  let arr_user = []
-  element.users.forEach(function(u_ele){
-    let u_li = document.createElement("li")
-    u_li.innerHTML = u_ele.username
-    u_ul.append(u_li)
-    arr_user.push(u_ele)
+function get(path){
+  return fetch(path).then(res => res.json())
+}
+
+function render_book_list(res){
+  res.forEach(function(book){
+    book_list.append(render_book(book))
   })
-  show.append(u_ul)
-  show.append(button)
-
-  show_list.append(show)
-  return arr_user
-
 }
 
-function patch(arr_user){
-  arr_user.push({"id": 1, "username": "pouros"})
-  let patchData = {
-      "users": arr_user
+function render_book(book){
+  let li = document.createElement("li")
+  li.innerHTML = book.title
+
+  li.addEventListener("click", function(){
+    render_book_info(book)
+  })
+  return li
+}
+
+function render_book_info(book){
+  show_list.innerHTML = `
+  <h2>${book.title}</h2>
+  <img src=${book.img_url}>
+  <p>${book.description}</p>
+  `
+  render_user_list(book)
+}
+
+function render_user_list(book){
+  let div_user = document.createElement("div")
+  div_user.id = "div_user"
+  book.users.forEach(function(user){
+    div_user.innerHTML += `<h4>${user.username}</h4>`
+  })
+  show_list.append(div_user)
+
+  let btn = document.createElement("button")
+  btn.id = "btn-read"
+  btn.innerHTML = "Read Book"
+
+  btn.addEventListener("click", function(){
+    makePatchRequest(book).then(res =>{
+      if(res !== 0){
+        let div = document.querySelector("#div_user")
+        div.innerHTML += `<h4>${res.users[res.users.length-1].username}</h4>`
+      }
+    })
+  })
+
+  show_list.append(btn)
+}
+
+function patch(user_arr){
+  let user = {"id":1, "username":"pouros"}
+  user_arr.push(user)
+  return {
+    "users": user_arr
+  }
+}
+
+function makePatchRequest(book){
+  var found = false;
+  for(var i = 0; i < book.users.length; i++) {
+    if (book.users[i].id == 1) {
+        found = true;
+        break;
     }
-  return patchData;
-}
+  }
 
-function get(url){
-  return fetch(url).then(res => res.json())
+  if(found){
+    alert("You read this already!");
+    return 0;
+  }
+  else{
+    return fetch(`${URL}/${book.id}`,{
+      method: 'PATCH',
+      body: JSON.stringify(patch(book.users)),
+      headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+    }).then(response => response.json())
+  }
 }
