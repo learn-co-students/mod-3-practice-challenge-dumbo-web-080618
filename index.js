f56cd9ad93652f126bce12b8e88a3c670467743c
joyhuangg <joyhuangg@gmail.com>
2018-09-26 22:31:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const URL = "http://localhost:3000/books"
+
+document.addEventListener("DOMContentLoaded", function() {
+  let adapter = new Adapter(URL);
+  let books = adapter.getBooks()
+    .then(showBooks)
+
+
+
+
+
+});
+
+function showBooks(books){
+  books.forEach(function(bookObj){
+    let book = new Book(bookObj)
+    book.renderBook()
+  })
+
+}
